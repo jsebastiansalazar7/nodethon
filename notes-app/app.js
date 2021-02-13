acf561e1002cc2f7d8688f9c830e93b84fd46ee3
J <js.salazar.aguirre@gmail.com>
2021-02-13 07:32:20 -0500

    Yargs command options added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,37 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require('./notes.js')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading note')
+    }
+})
+
+yargs.parse()
