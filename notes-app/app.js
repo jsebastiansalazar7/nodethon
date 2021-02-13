554ef9877f61c7be13fade64edd1a796e5ba7f23
J <js.salazar.aguirre@gmail.com>
2021-02-13 08:02:33 -0500

    Added builder to the read command

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,44 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require('./notes.js')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        console.log('Listing notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Reading note', argv.title)
+    }
+})
+
+yargs.parse()
