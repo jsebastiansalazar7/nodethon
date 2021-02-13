9272bfc48d9c2385fc5e336b3af8e3786431f65c
jsebastiansalazar7 <39344265+jsebastiansalazar7@users.noreply.github.com>
2021-02-12 20:24:22 -0500

    Notes app first steps (#2)
    Added main scripts

---
+++ notes-app/notes.js
@@ -0,0 +1,13 @@
+const getNotes = function() {
+    return 'Your notes...'
+}
+
+const addNotes = function() {
+    // TODO: Add notes
+    return 'Notes added'
+}
+
+module.exports = {
+    getNotes, 
+    addNotes
+}
